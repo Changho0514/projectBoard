79d98199e671ad3b74d4bf7ddeca34942658e051
Changho Choi <chh9450@naver.com>
2023-06-19 15:58:16 +0900

    #40 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    id -> userId로 id를 변경하는 내용을
    모든 자바코드에 반영

--- fastcampus-project-board/src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
@@ -8,6 +8,7 @@ import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.projectboard.dto.UserAccountDto;
 import com.fastcampus.projectboard.repository.ArticleRepository;
 import com.fastcampus.projectboard.repository.UserAccountRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -234,20 +235,21 @@ class ArticleServiceTest {
         then(articleRepository).should().deleteById(articleId);
     }
     
-    @DisplayName("게시글 수를 조회하면, 게시글 수를 반환한다")
-    @Test
-    void givenNothing_whenCountingArticles_thenReturnsArticleCount() {
-        // Given
-        long expected = 0L;
-        given(articleRepository.count()).willReturn(expected);
-        
-        // When
-        long actual = sut.getArticleCount();
-        
-        // Then
-        assertThat(actual).isEqualTo(expected);
-        then(articleRepository).should().count();
-    }
+//    @Disabled("개발중")
+//    @DisplayName("게시글 수를 조회하면, 게시글 수를 반환한다")
+//    @Test
+//    void givenNothing_whenCountingArticles_thenReturnsArticleCount() {
+//        // Given
+//        long expected = 0L;
+//        given(articleRepository.count()).willReturn(expected);
+//
+//        // When
+//        long actual = sut.getArticleCount();
+//
+//        // Then
+//        assertThat(actual).isEqualTo(expected);
+//        then(articleRepository).should().count();
+//    }
     
     @DisplayName("해시태그를 조회하면, 유니크 해시태그 리스트를 반환한다")
     @Test
